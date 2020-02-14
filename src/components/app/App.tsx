7ef9631540fbf0910bdb534f344c47befa98502d
Eugene <gk16121983@gmail.com>
2020-02-14 19:52:27 +0200

    refactor App.tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,44 +1,26 @@
 import React, {useState} from 'react'
 import './App.scss'
-import AppHeader from '../app-header/AppHeader'
-import Main from '../main/Main'
-import Login from '../authorization/Login'
-import Forgot from '../authorization/Forgot'
-import SignUp from '../authorization/SignUp'
-import {ProjectContext} from '../../services/ProjectContext'
-import dashboardApiServices from "../../services/DashboardServices";
-
+import {BrowserRouter, Switch, Route}from 'react-router-dom'
+import ProjectsPage from '../../pages/ProjectsPage'
+import InboxPage from '../../pages/InboxPage'
+import HomePage from '../../pages/HomePage'
+import RaportPage from '../../pages/RaportPage'
+import AuthorizationPage from '../../pages/AuthorizationPage'
 
 const App = () => {
-    dashboardApiServices
-        .getAllProjects()
-        .then(response => response.json())
-        .then(response =>  localStorage.setItem('projects', JSON.stringify(response)))
-        .catch(err => console.error(err))
-    const [projects, changeProjects] = useState<any>(JSON.parse(localStorage.getItem('projects') as any) || [])
-
-    const projectsHandler = ():void => {
-        dashboardApiServices
-            .getAllProjects()
-            .then(response => response.json())
-            .then(response =>  {
-                localStorage.setItem('projects', JSON.stringify(response))
-                changeProjects(JSON.parse(localStorage.getItem('projects') as any))
-            })
-            .catch(err => console.error(err))
-
-    }
 
     return (
-        <div className="App">
-            <ProjectContext.Provider value={projectsHandler}>
-                <AppHeader />
-                <Main projectsItems={projects}/>
-            </ProjectContext.Provider>
-            {/*<Login/>*/}
-            {/*<Forgot/>*/}
-            {/*<SignUp/>*/}
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Switch>
+                    <Route component={HomePage} path="/" exact/>
+                    <Route  component={ProjectsPage} path="/projects"/>
+                    <Route  component={RaportPage} path="/raport"/>
+                    <Route  component={InboxPage} path="/inbox"/>
+                    <Route  component={AuthorizationPage} path="/login"/>
+                </Switch>
+            </div>
+        </BrowserRouter>
     )
 }
 
