ae2d59614c646f0f435205e7806ae926e601e78f
Eugene <gk16121983@gmail.com>
2020-02-14 19:51:24 +0200

    refactor Main.tsx

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -3,10 +3,10 @@ import './Main.scss'
 import Aside from '../aside/Aside'
 import Middle from '../middle/Middle'
 import {IProjectsItem} from '../../interfaces/IProjectsItem'
+import {asideMenuIcons} from '../../services/ProjectContext'
 
 
 const Main:React.FC<IProjectsItem> = props => {
-    const asideMenuIcons: Array<string> = ['home', 'bars', 'chart-line', 'envelope', 'user-friends']
     return (
         <main className="main">
             <Aside asideMenuIcons={asideMenuIcons}/>
