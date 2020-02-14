c5b5e00dc8c7e046d623ef953b2153b904f564c5
Eugene <gk16121983@gmail.com>
2020-02-14 19:52:07 +0200

    refactor ProjectContext.ts

--- src/services/ProjectContext.ts
+++ src/services/ProjectContext.ts
@@ -1,3 +1,5 @@
 import {createContext} from 'react'
 
 export const ProjectContext = createContext<any>([])
+
+export const asideMenuIcons: Array<string> = ['home', 'bars', 'chart-line', 'envelope', 'user-friends']
