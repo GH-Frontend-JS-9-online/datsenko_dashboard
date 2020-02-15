d73f4f3d6d76185b0cd4b322370e618bbf8a9ad3
Eugene <gk16121983@gmail.com>
2020-02-15 18:04:50 +0200

    feature InboxMenu.tsx, InboxMenu.scss

---
+++ src/components/inbox-menu/InboxMenu.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './InboxMenu.scss'
+import {IMessagesItem} from '../../interfaces/IMessagesItem'
+
+const InboxMenu:React.FC<IMessagesItem> = props => {
+    return (
+        <nav className="inbox-nav">
+            <ul className="inbox-nav__menu">
+                <ul className="inbox-nav__left-menu">
+                    <li className="inbox-nav__left-menu__item">
+                        <a href="#" className="inbox-nav__left-menu__link">
+                            <i className="fas fa-inbox"></i>
+                            {`Inbox (${props.messagesItems.length})`}
+                        </a>
+                    </li>
+                    <li className="inbox-nav__left-menu__item">
+                        <a href="#" className="inbox-nav__left-menu__link">
+                            <i className="fas fa-paper-plane"></i>
+                            Sent
+                        </a>
+                    </li>
+                    <li className="inbox-nav__left-menu__item">
+                        <a href="#" className="inbox-nav__left-menu__link">
+                            <i className="fas fa-trash"></i>
+                            Trash
+                        </a>
+                    </li>
+                </ul>
+                <ul className="inbox-nav__right-menu">
+                    <li className="inbox-nav__right-menu__item">
+                        <a href="#" className="inbox-nav__right-menu__link">
+                            Filter messages:
+                        </a>
+                    </li>
+                    <li className="inbox-nav__right-menu__item">
+                        <a href="#" className="inbox-nav__right-menu__link">
+                            Date
+                        </a>
+                    </li>
+                </ul>
+            </ul>
+        </nav>
+    )
+}
+
+export default InboxMenu
