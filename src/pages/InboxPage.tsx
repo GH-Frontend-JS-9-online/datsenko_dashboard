6d2bef3860be542dce40a283e66c1043327d9e00
Eugene <gk16121983@gmail.com>
2020-02-15 18:05:54 +0200

    feature InboxPage.tsx

--- src/pages/InboxPage.tsx
+++ src/pages/InboxPage.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import AppHeader from '../components/app-header/AppHeader'
-import {asideMenuIcons, ProjectContext} from '../services/ProjectContext'
+import {asideMenuIcons} from '../services/ProjectContext'
 import Aside from '../components/aside/Aside'
+import InboxMenu from '../components/inbox-menu/InboxMenu'
+import dashboardApiServices from '../services/DashboardServices'
 
 
 const InboxPage:React.FC = () => {
+
+    dashboardApiServices
+        .allThreadMessages()
+        .then(response => response.json())
+        .then(response =>  localStorage.setItem('allThreadMessages', JSON.stringify(response)))
+        .catch(err => console.error(err))
+
     return (
         <div>
             <AppHeader/>
             <main className="main">
                 <Aside asideMenuIcons={asideMenuIcons}/>
-                <h1 style={{color: '#fff'}}>InboxPage</h1>
+                <div className="middle">
+                    <InboxMenu messagesItems={JSON.parse(localStorage.getItem('allThreadMessages') as string)}/>
+                </div>
+
             </main>
         </div>
     )
