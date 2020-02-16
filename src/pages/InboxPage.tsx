e57f4fe551973063fc8dbde4e06f8490653ec487
Eugene <gk16121983@gmail.com>
2020-02-16 13:26:47 +0200

    refactor InboxPage.tsx

--- src/pages/InboxPage.tsx
+++ src/pages/InboxPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import AppHeader from '../components/app-header/AppHeader'
 import {asideMenuIcons} from '../services/ProjectContext'
 import Aside from '../components/aside/Aside'
@@ -7,28 +7,31 @@ import dashboardApiServices from '../services/DashboardServices'
 import Inbox from "../components/inbox/Inbox";
 import Coversation from "../components/coversation/Coversation";
 import About from "../components/about/About";
-
+import {MessageContext} from '../services/MessageContext'
 
 const InboxPage:React.FC = () => {
-
+    const [messages, updateMessages] = useState([...JSON.parse(localStorage.getItem('allThreadMessages') as any)])
     dashboardApiServices
         .allThreadMessages()
         .then(response => response.json())
         .then(response =>  localStorage.setItem('allThreadMessages', JSON.stringify(response)))
         .catch(err => console.error(err))
-
+    console.log(messages)
     return (
         <div>
             <AppHeader/>
             <main className="main">
                 <Aside asideMenuIcons={asideMenuIcons}/>
                 <div className="middle">
-                    <InboxMenu messagesItems={JSON.parse(localStorage.getItem('allThreadMessages') as string)}/>
-                    <div className="message">
-                        <Inbox/>
-                        <Coversation/>
-                        <About aboutItems={[JSON.parse(localStorage.getItem('user') as any)]}/>
-                    </div>
+                    <MessageContext.Provider value={{messages, updateMessages}}>
+                        <InboxMenu messagesItems={messages}/>
+                        <div className="message">
+                            <Inbox/>
+                            <Coversation/>
+                            <About aboutItems={[JSON.parse(localStorage.getItem('user') as any)]}/>
+                        </div>
+                    </MessageContext.Provider>
+
                 </div>
 
             </main>
