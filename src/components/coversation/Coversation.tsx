d7255d75d2c7d27aaacb14f1f888851d25b67877
Eugene <gk16121983@gmail.com>
2020-02-16 12:56:55 +0200

    feature Coversation.tsx, Coversation.scss

---
+++ src/components/coversation/Coversation.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Coversation.scss'
+import FormForMessage from '../form-for-message/FormForMessage'
+
+const Coversation:React.FC = () => {
+    return (
+        <div className="coversation">
+            <FormForMessage/>
+        </div>
+    )
+}
+
+export default Coversation
