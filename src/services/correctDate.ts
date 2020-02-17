50319e98b0e5b172a368c7a7b1dd2c4ffe77163d
Eugene <gk16121983@gmail.com>
2020-02-17 21:38:58 +0200

    feature correctDate.ts

---
+++ src/services/correctDate.ts
@@ -0,0 +1,8 @@
+const correctDate = (): string => {
+    let date = new Date()
+    let hours: string = (date.getHours() < 10) ? `0${date.getHours()}` : `${date.getHours()}`
+    let minutes: string = (date.getMinutes() < 10) ? `0${date.getMinutes()}` : `${date.getMinutes()}`
+    return `${hours}:${minutes}`
+}
+
+export default correctDate
