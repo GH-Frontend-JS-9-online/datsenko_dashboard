de06c63b6d36199412bf9c5655527995beca9efc
Eugene <gk16121983@gmail.com>
2020-02-17 21:38:37 +0200

    feature Messages.tsx, Messages.scss

---
+++ src/components/messages/Messages.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Messages.scss'
+import correctDate from '../../services/correctDate'
+
+
+const Messages: React.FC = () => {
+    return (
+        <ul className="show-messages">
+            <li className="show-messages__item">
+                <i className="fas fa-circle"></i>
+                <div>
+                    <p>
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ulla
+                        pariatur.
+                    </p>
+                    <span>{correctDate()}</span>
+                </div>
+            </li>
+            <li className="show-messages__item right">
+                <i className="fas fa-circle"></i>
+                <div>
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ulla
+                        pariatur.
+                    </p>
+                    <span>4 April 2016, 5:32 PM</span>
+                </div>
+            </li>
+        </ul>
+    )
+}
+
+export default Messages
