b2d80910e88075a69f472cd2ecca49001cb638bb
Eugene <gk16121983@gmail.com>
2020-02-17 16:33:56 +0200

    feature store.ts

---
+++ src/store/store.ts
@@ -0,0 +1,12 @@
+import {createStore} from 'redux'
+import rootReducer from '../reducers/reducers'
+
+let initialState: any = {
+    user: {...JSON.parse(<any>localStorage.getItem('user'))},
+    projects: [...JSON.parse(<any>localStorage.getItem('projects'))],
+    allThreadMessages: [...JSON.parse(<any>localStorage.getItem('allThreadMessages'))]
+}
+
+const store = createStore(rootReducer, initialState)
+
+export default store
