d9a0989d4bca27186bc6d8f3a604d9dfa3b94f08
Eugene <gk16121983@gmail.com>
2020-02-17 21:41:12 +0200

    refactor Coversation.tsx

--- src/components/coversation/Coversation.tsx
+++ src/components/coversation/Coversation.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Coversation.scss'
 import FormForMessage from '../form-for-message/FormForMessage'
+import Messages from '../messages/Messages'
 
 const Coversation:React.FC = () => {
     return (
         <div className="coversation">
+            <Messages/>
             <FormForMessage/>
         </div>
     )
