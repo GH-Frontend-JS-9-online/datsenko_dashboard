4760f70c79cc6f3c42e6239dde0ca341c0c98b27
Eugene <gk16121983@gmail.com>
2020-02-21 18:14:50 +0200

    refactor ProjectsPage.tsx

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,34 +1,19 @@
-import React, {useState} from 'react'
-import {ProjectContext} from '../services/ProjectContext'
+import React from 'react'
 import AppHeader from '../components/app-header/AppHeader'
 import Main from '../components/main/Main'
 import dashboardApiServices from '../services/DashboardServices'
 
-const ProjectsPage:React.FC = () => {
+const ProjectsPage: React.FC = () => {
     dashboardApiServices
         .getAllProjects()
         .then(response => response.json())
-        .then(response =>  localStorage.setItem('projects', JSON.stringify(response)))
+        .then(response => localStorage.setItem('projects', JSON.stringify(response)))
         .catch(err => console.error(err))
-    const [projects, changeProjects] = useState<any>(JSON.parse(localStorage.getItem('projects') as any) || [])
 
-    const projectsHandler = ():void => {
-        dashboardApiServices
-            .getAllProjects()
-            .then(response => response.json())
-            .then(response =>  {
-                localStorage.setItem('projects', JSON.stringify(response))
-                changeProjects(JSON.parse(localStorage.getItem('projects') as any))
-            })
-            .catch(err => console.error(err))
-
-    }
     return (
         <div>
-            <ProjectContext.Provider value={projectsHandler}>
-                <AppHeader />
-                <Main projectsItems={projects}/>
-            </ProjectContext.Provider>
+            <AppHeader/>
+            <Main/>
         </div>
     )
 }
