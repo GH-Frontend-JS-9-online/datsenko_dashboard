b121a3cb0c5109651b814aa6ecba8ef4e0985538
Eugene <gk16121983@gmail.com>
2020-02-21 18:13:16 +0200

    refactor HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import AppHeader from '../components/app-header/AppHeader'
 import Aside from '../components/aside/Aside'
-import {asideMenuIcons} from '../services/ProjectContext'
 
 const HomePage:React.FC = () => {
     return (
         <div>
             <AppHeader/>
             <main className="main">
-                <Aside asideMenuIcons={asideMenuIcons}/>
+                <Aside/>
                 <h1 style={{color: '#fff'}}>HomePage</h1>
             </main>
         </div>
