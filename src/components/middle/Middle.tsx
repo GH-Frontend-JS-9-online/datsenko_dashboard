cb8e7827cce4fffbe71d9383d4b8593840da3555
Eugene <gk16121983@gmail.com>
2020-02-21 18:15:55 +0200

    refactor Middle.tsx

--- src/components/middle/Middle.tsx
+++ src/components/middle/Middle.tsx
@@ -3,26 +3,14 @@ import './Middle.scss'
 import SecondMenu from '../second-menu/SecondMenu'
 import ProjectMenu from '../project-menu/ProjectMenu'
 import ProjectsItem from '../projects-item/ProjectsItem'
-import {IProjectsItem} from '../../interfaces/IProjectsItem'
 
-const Middle:React.FC<IProjectsItem> = props => {
-
-    const projectMenuItems: Array<string> = [
-        'Project title',
-        'Value',
-        'Deadline',
-        'Time spent',
-        'Progress',
-        'Status',
-        'Assigned to',
-        ''
-    ]
+const Middle:React.FC = () => {
 
     return (
         <div className="middle">
-            <SecondMenu projectsItems={props.projectsItems}/>
-            <ProjectMenu projectMenuItems={projectMenuItems}/>
-            <ProjectsItem projectsItems={props.projectsItems}/>
+            <SecondMenu />
+            <ProjectMenu/>
+            <ProjectsItem />
         </div>
     )
 }
