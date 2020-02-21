04728767183bea302ef0901545069cab6ae9e3dc
Eugene <gk16121983@gmail.com>
2020-02-21 18:17:50 +0200

    refactor MessageContext.ts, ProjectContext.ts

--- src/services/MessageContext.ts
+++
@@ -1,3 +0,0 @@
-import {createContext} from 'react'
-
-export const MessageContext = createContext<any>([...JSON.parse(localStorage.getItem('allThreadMessages') as any)])
--- src/services/ProjectContext.ts
+++
@@ -1,5 +0,0 @@
-import {createContext} from 'react'
-
-export const ProjectContext = createContext<any>([])
-
-export const asideMenuIcons: Array<string> = ['home', 'bars', 'chart-line', 'envelope', 'user-friends']
