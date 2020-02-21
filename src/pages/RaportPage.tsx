cea0892cec24ca1d14aaffd9edaad80c2afe491a
Eugene <gk16121983@gmail.com>
2020-02-21 18:13:34 +0200

    refactor RaportPage.tsx

--- src/pages/RaportPage.tsx
+++ src/pages/RaportPage.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import AppHeader from '../components/app-header/AppHeader'
 import Aside from '../components/aside/Aside'
-import {asideMenuIcons} from '../services/ProjectContext'
 
 const RaportPage:React.FC = () => {
     return (
         <div>
             <AppHeader/>
             <main className="main">
-                <Aside asideMenuIcons={asideMenuIcons}/>
+                <Aside/>
                 <h1 style={{color: '#fff'}}>RaportPage</h1>
             </main>
         </div>
