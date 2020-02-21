22ed89150ec1a0420121cac1fe0c5db89867b8fb
Eugene <gk16121983@gmail.com>
2020-02-21 16:33:58 +0200

    refactor store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,9 +2,9 @@ import {createStore} from 'redux'
 import rootReducer from '../reducers/reducers'
 
 let initialState: any = {
-    user: [{...JSON.parse(<any>localStorage.getItem('user'))}],
-    projects: [...JSON.parse(<any>localStorage.getItem('projects'))],
-    allThreadMessages: [...JSON.parse(<any>localStorage.getItem('allThreadMessages'))]
+    user: [{...JSON.parse(<any>localStorage.getItem('user'))} || {}],
+    projects: [...JSON.parse(<any>localStorage.getItem('projects')) || []],
+    allThreadMessages: [...JSON.parse(<any>localStorage.getItem('allThreadMessages')) || []]
 }
 
 const store = createStore(rootReducer, initialState)
