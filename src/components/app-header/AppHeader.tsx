f68fc50fa181ed57807ae113849aa850bdcfa726
Eugene <gk16121983@gmail.com>
2020-02-22 00:34:42 +0200

    refactor AppHeader.tsx

--- src/components/app-header/AppHeader.tsx
+++ src/components/app-header/AppHeader.tsx
@@ -2,15 +2,16 @@ import React from 'react'
 import './AppHeader.scss'
 import TopMenu from '../top-menu/TopMenu'
 import logo from '../../assets/images/logo.svg'
+import {NavLink} from 'react-router-dom'
 
 const AppHeader:React.FC = () => {
     const topMenuIcons: Array<string> = ['plus', 'search', 'bell', 'circle']
     return (
         <header className="header">
             <nav className="top-nav">
-                <a href="#" className="top-nav__logo">
+                <NavLink to="/" className="top-nav__logo">
                     <img src={logo} alt="logo"/>
-                </a>
+                </NavLink>
                 <TopMenu topMenuIcons={topMenuIcons}/>
             </nav>
         </header>
