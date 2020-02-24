509bdbc71e0da5ded45ec27f36305a7033b722ec
Eugene <gk16121983@gmail.com>
2020-02-24 21:13:35 +0200

    refactor reducers.ts

--- src/reducers/reducers.ts
+++ src/reducers/reducers.ts
@@ -1,4 +1,5 @@
 enum ActionsDashboard {
+    Login = 'IS_LOGIN',
     User = 'UPDATE_USER',
     Projects = 'UPDATE_PROJECTS',
     Messages = 'UPDATE_MESSAGES'
@@ -6,16 +7,33 @@ enum ActionsDashboard {
 
 export const rootReducer = (state: any, action: any) => {
     switch (action.type) {
+        case  ActionsDashboard.Login:
+            return {
+                ...state,
+                isLogin: action.payload
+            }
         case ActionsDashboard.User:
             return {}
         case ActionsDashboard.Projects:
-            return {...state,  projects: [...state.projects, action.payload]}
+            return {
+                ...state,
+                projects: [...state.projects, action.payload]
+            }
         case ActionsDashboard.Messages:
-            return {...state, allThreadMessages: [...state.allThreadMessages, action.payload]}
+            return {
+                ...state,
+                allThreadMessages: [...state.allThreadMessages, action.payload]
+            }
         default:
             return state
     }
 }
+
+export const isLogin = (isLogin: boolean) => ({
+    type: ActionsDashboard.Login,
+    payload: isLogin
+})
+
 
 export const updateProjects = (project: any) => ({
     type: ActionsDashboard.Projects,
