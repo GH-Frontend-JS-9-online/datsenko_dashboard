27e34e06d980662e42197438bf943a71e05534ee
Eugene <gk16121983@gmail.com>
2020-02-25 19:06:11 +0200

    refactor RaportPage.tsx

--- src/pages/RaportPage.tsx
+++ src/pages/RaportPage.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import AppHeader from '../components/app-header/AppHeader'
 import Aside from '../components/aside/Aside'
+import RaportMenu from '../components/raport-menu/RaportMenu'
+import RaportChart from '../components/raport-chart/RaportChart'
+import RaportTable from '../components/raport-table/RaportTable'
 
 const RaportPage:React.FC = () => {
     return (
@@ -8,7 +11,11 @@ const RaportPage:React.FC = () => {
             <AppHeader/>
             <main className="main">
                 <Aside/>
-                <h1 style={{color: '#fff'}}>RaportPage</h1>
+                <div className="raport" style={{width: '100%'}}>
+                    <RaportMenu/>
+                    <RaportChart/>
+                    <RaportTable/>
+                </div>
             </main>
         </div>
     )
