3f26b01ee7c6c65cda05ddd159a5d20e4669c87b
Eugene <gk16121983@gmail.com>
2020-02-26 10:53:51 +0200

    refactor RaportMenu.tsx

--- src/components/raport-menu/RaportMenu.tsx
+++ src/components/raport-menu/RaportMenu.tsx
@@ -1,28 +1,78 @@
 import React from 'react'
-import { CircularProgressbar } from 'react-circular-progressbar'
+import { CircularProgressbar, buildStyles  } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
 import './RaportMenu.scss'
+import VisibilitySensor from 'react-visibility-sensor'
+
 
 const RaportMenu:React.FC = () => {
     return (
         <nav className="raport-nav">
             <ul className="raport-nav__menu">
                 <li className="raport-nav__menu__item">
-                    <CircularProgressbar value={75} text={`${75}%`} />
+                    <VisibilitySensor>
+                        {({ isVisible }) => {
+                            const percentage = isVisible ? 75 : 0;
+                            return (
+                                <CircularProgressbar
+                                    value={percentage}
+                                    text={`${percentage}%`}
+                                    styles={buildStyles({
+                                        textSize: '22px',
+                                        pathColor: `#2196f3`,
+                                        textColor: '#2196f3',
+                                        trailColor: '#9ca1b2'
+                                    })}
+                                />
+                            );
+                        }}
+                    </VisibilitySensor>
                     <div className="visitors">
                         <h3>1 300</h3>
                         <p>VIEWS</p>
                     </div>
                 </li>
                 <li className="raport-nav__menu__item">
-                    <CircularProgressbar value={35} text={`${35}%`} />
+                    <VisibilitySensor>
+                        {({ isVisible }) => {
+                            const percentage = isVisible ? 35 : 0;
+                            return (
+                                <CircularProgressbar
+                                    value={percentage}
+                                    text={`${percentage}%`}
+                                    styles={buildStyles({
+                                        textSize: '22px',
+                                        pathColor: `#2196f3`,
+                                        textColor: '#2196f3',
+                                        trailColor: '#9ca1b2'
+                                    })}
+                                />
+                            );
+                        }}
+                    </VisibilitySensor>
                     <div className="visitors">
                         <h3>800</h3>
                         <p>VISITORS</p>
                     </div>
                 </li>
                 <li className="raport-nav__menu__item">
-                    <CircularProgressbar value={62} text={`${62}%`} />
+                    <VisibilitySensor>
+                        {({ isVisible }) => {
+                            const percentage = isVisible ? 62 : 0;
+                            return (
+                                <CircularProgressbar
+                                    value={percentage}
+                                    text={`${percentage}%`}
+                                    styles={buildStyles({
+                                        textSize: '22px',
+                                        pathColor: `#2196f3`,
+                                        textColor: '#2196f3',
+                                        trailColor: '#9ca1b2'
+                                    })}
+                                />
+                            );
+                        }}
+                    </VisibilitySensor>
                     <div className="visitors">
                         <h3>3 800</h3>
                         <p>IMPRESSIONS</p>
