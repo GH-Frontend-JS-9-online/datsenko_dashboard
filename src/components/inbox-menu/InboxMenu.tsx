eecbdf6056459aa979013ad555345b5484aa8231
Eugene <gk16121983@gmail.com>
2020-02-27 19:19:03 +0200

    refactor InboxMenu.tsx, InboxMenu.scss

--- src/components/inbox-menu/InboxMenu.tsx
+++ src/components/inbox-menu/InboxMenu.tsx
@@ -1,45 +1,33 @@
 import React from 'react'
 import './InboxMenu.scss'
 import {useSelector} from 'react-redux'
+import ButtonOptions from '../button-options/ButtonOptions'
 
-const InboxMenu:React.FC = () => {
+const InboxMenu: React.FC = () => {
     const allThreadMessages = useSelector((state: any) => state.allThreadMessages)
     return (
         <nav className="inbox-nav">
-            <ul className="inbox-nav__menu">
-                <ul className="inbox-nav__left-menu">
-                    <li className="inbox-nav__left-menu__item">
-                        <a href="#" className="inbox-nav__left-menu__link">
-                            <i className="fas fa-inbox"></i>
-                            {`Inbox (${allThreadMessages.length})`}
-                        </a>
-                    </li>
-                    <li className="inbox-nav__left-menu__item">
-                        <a href="#" className="inbox-nav__left-menu__link">
-                            <i className="fas fa-paper-plane"></i>
-                            Sent
-                        </a>
-                    </li>
-                    <li className="inbox-nav__left-menu__item">
-                        <a href="#" className="inbox-nav__left-menu__link">
-                            <i className="fas fa-trash"></i>
-                            Trash
-                        </a>
-                    </li>
-                </ul>
-                <ul className="inbox-nav__right-menu">
-                    <li className="inbox-nav__right-menu__item">
-                        <a href="#" className="inbox-nav__right-menu__link">
-                            Filter messages:
-                        </a>
-                    </li>
-                    <li className="inbox-nav__right-menu__item">
-                        <a href="#" className="inbox-nav__right-menu__link">
-                            Date
-                        </a>
-                    </li>
-                </ul>
+            <ul className="inbox-nav_menu">
+                <li className="inbox-nav_menu__item">
+                    <a href="#" className="inbox-nav_menu__link">
+                        <i className="fas fa-inbox"></i>
+                        {`Inbox (${allThreadMessages.length})`}
+                    </a>
+                </li>
+                <li className="inbox-nav_menu__item">
+                    <a href="#" className="inbox-nav_menu__link">
+                        <i className="fas fa-paper-plane"></i>
+                        Sent
+                    </a>
+                </li>
+                <li className="inbox-nav_menu__item">
+                    <a href="#" className="inbox-nav_menu__link">
+                        <i className="fas fa-trash"></i>
+                        Trash
+                    </a>
+                </li>
             </ul>
+            <ButtonOptions beforeButtonText="Filter messages" buttonName="Date"/>
         </nav>
     )
 }
