20bf6535186d7f194deb63eb23e71c07b5faee69
Eugene <gk16121983@gmail.com>
2020-02-27 19:18:20 +0200

    refactor SecondMenu.tsx, SecondMenu.scss

--- src/components/second-menu/SecondMenu.tsx
+++ src/components/second-menu/SecondMenu.tsx
@@ -1,37 +1,26 @@
 import React from 'react'
 import './SecondMenu.scss'
 import {useSelector} from 'react-redux'
+import ButtonOptions from '../button-options/ButtonOptions'
 
 const SecondMenu:React.FC = () => {
     const projects = useSelector((state: any) => state.projects)
     return (
         <nav className="second-nav">
-            <ul className="second-nav__menu">
-                <ul className="second-nav__left-menu">
-                    <li className="second-nav__left-menu__item">
-                        <a href="#" className="second-nav__left-menu__link">
+            <ul className="second-nav_menu">
+
+                    <li className="second-nav_menu__item">
+                        <a href="#" className="second-nav_menu__link">
                             {`All Projects (${projects.length})`}
                         </a>
                     </li>
-                    <li className="second-nav__left-menu__item">
-                        <a href="#" className="second-nav__left-menu__link">
+                    <li className="second-nav_menu__item">
+                        <a href="#" className="second-nav_menu__link">
                             Workflow
                         </a>
                     </li>
-                </ul>
-                <ul className="second-nav__right-menu">
-                    <li className="second-nav__right-menu__item">
-                        <a href="#" className="second-nav__right-menu__link">
-                            Show projects:
-                        </a>
-                    </li>
-                    <li className="second-nav__right-menu__item">
-                        <a href="#" className="second-nav__right-menu__link">
-                            All
-                        </a>
-                    </li>
-                </ul>
             </ul>
+            <ButtonOptions beforeButtonText="Show projects" buttonName="All"/>
         </nav>
     )
 }
