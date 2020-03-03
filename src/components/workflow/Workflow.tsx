3ccbb30aaa1ba281929da7efb5c629e4603c2b26
Eugene <gk16121983@gmail.com>
2020-03-03 20:54:15 +0200

    refactor Workflow.tsx

--- src/components/workflow/Workflow.tsx
+++ src/components/workflow/Workflow.tsx
@@ -1,6 +1,7 @@
 import React, {useRef} from 'react'
 import './Workflow.scss'
 import useDraggable from './useDraggable'
+import Card from './Card'
 
 const DraggableCard: React.FC = ({ children }) => {
     const cardRef = useRef(null)
@@ -16,15 +17,24 @@ const DraggableCard: React.FC = ({ children }) => {
 const Workflow:React.FC = () => {
     return (
         <div className="workflow__container">
-            <DraggableCard>Card 1</DraggableCard>
-            <DraggableCard>Card 2</DraggableCard>
-            <DraggableCard>Card 3</DraggableCard>
-            <DraggableCard>Card 4</DraggableCard>
-            <DraggableCard>Card 5</DraggableCard>
-            <DraggableCard>Card 6</DraggableCard>
-            <DraggableCard>Card 7</DraggableCard>
-            <DraggableCard>Card 8</DraggableCard>
-            <DraggableCard>Card 9</DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
+            <DraggableCard><Card/></DraggableCard>
         </div>
     )
 }
