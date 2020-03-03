1cb4275ad713fa7ffd7948476cb26130edb5f3ee
Eugene <gk16121983@gmail.com>
2020-03-03 19:09:11 +0200

    feature MiniPresentation.tsx, MiniPresentation.scss

---
+++ src/components/board-mini-presentation/MiniPresentation.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './MiniPresentation.scss'
+import RaportChart from '../raport-chart/RaportChart'
+import MiniRaport from './MiniRaport'
+import HomeBarChart from '../home-bar-chart/HomeBarChart'
+import Calendar from 'react-calendar'
+
+const MiniPresentation: React.FC = () => {
+    return (
+        <div className="mini-presentation">
+            <div className="mini-raport">
+                <MiniRaport/>
+                <RaportChart/>
+            </div>
+            <div className="mini-projects">
+
+            </div>
+
+            <div className="wrapper-sales-inbox">
+                <div className="sales-raport">
+                    <HomeBarChart/>
+                </div>
+                <div className="mini-inbox"></div>
+            </div>
+
+            <div className="calendar">
+                <Calendar
+                    calendarType={'US'}
+                    locale={'en-EN'}
+                    next2Label={''}
+                    prev2Label={''}
+
+                />
+            </div>
+        </div>
+    )
+}
+
+export default MiniPresentation
